ffe9e2209e5c292c28a65dc7520a5cc90f3bf9f9
arshptl <arshpatel714@gmail.com>
2022-08-31 19:37:49 +0530

    login mutation-50%

--- src/schema/user.schema.ts
+++ src/schema/user.schema.ts
@@ -9,4 +9,14 @@ export const createUserOutputSchema = z.object({
   name: z.string(),
   email: z.string().email(),
 });
+
+//TYPE
 export type createUserInput = z.TypeOf<typeof createUserSchema>;
+
+export const requestOtpSchema = z.object({
+  email: z.string().email(),
+  redirect: z.string().default("/"),
+});
+
+//TYPE
+export type requestOtpInput = z.TypeOf<typeof requestOtpSchema>;
